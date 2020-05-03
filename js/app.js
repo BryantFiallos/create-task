06fab300cbc18f0742b708f563d05fb05069926f
unknown <bryant.fiallos@gmail.com>
2020-05-02 21:39:28 -0400

    commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+//CONSTANTS
+
+const easyWords = ["dog", "cat", "bat", "axe", "hat", "ant", "sat", "rug", "mug", "bet"];
+const mediumWords = ["house", "beach", "adult", "chief", "funny", "hello", "metal", "sauce", "cocoa", "flags", "upset", "pearl", "trash", "enemy", "pizza", "humor", "eagle", "flame", "cargo", "puppy", "retro", "spark", "bride", "straw", "inbox", "bored", "chalk", "fatal", "hobby", "melee", "bagel", "debug", "amaze", "witch", "stool", "thief", "acorn", "hover", "lever", "merge", "lunar", "debit", "cubic", "erase", "vivid", "waist", "yeast", "syrup", "trunk", "rebel", "lobby", "pasta", "grape", "choir", "jewel", "scoop", "rival", "yacht", "sushi", "bunny"]
+
+//VARIABLES
+
+let turboTypingArray = [];
+let word
+let score = 0;
+let attempts = 0;
+let correctAttempts = 0;
+let answerStreak = 0;
+
+//EVENT LISTENERS
+
 var input = document.getElementById("user-input");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
@@ -6,23 +22,16 @@ input.addEventListener("keyup", function(event) {
  };
 });
 
-let turboTypingArray = [];
-const easyWords = ["dog", "cat", "bat", "axe", "hat", "ant", "sat", "rug", "mug", "bet"];
-let word
-
-
-let score = 0;
-let attempts = 0;
-let correctAttempts = 0;
-
-
 document.getElementById("easy").onclick = setEasy;
+document.getElementById("medium").onclick = setMedium;
 
+//FUNCTIONS
 
 function init() {
   score = 0;
   attempts = 0;
   correctAttempts = 0;
+  answerStreak = 0;
 }
 
 function refresh() {
@@ -39,13 +48,32 @@ function refresh() {
 
 function setEasy() {
 
+init();
+
   turboTypingArray = easyWords.slice();
 
   turboTypingArray.forEach((item, i) => {
     document.getElementById("board").innerHTML += "<div class='board-word'>" +item + "</div>"
   });
   console.log(turboTypingArray);
+  refresh();
 }
+
+
+
+function setMedium() {
+
+  init();
+
+    turboTypingArray = mediumWords.slice();
+
+    turboTypingArray.forEach((item, i) => {
+      document.getElementById("board").innerHTML += "<div class='board-word'>" +item + "</div>"
+    });
+    console.log(turboTypingArray);
+    refresh();
+}
+
 
 
 function CheckInput() {
@@ -70,6 +98,7 @@ function correctWord() {
   score = score + 10;
   attempts = attempts + 1;
   correctAttempts = correctAttempts + 1;
+  answerStreak = answerStreak + 1;
 
   for (var i = 0; i < turboTypingArray.length; i++) {
     if(turboTypingArray[i] == word) {
@@ -83,6 +112,7 @@ function correctWord() {
 
 function wrongWord() {
   attempts = attempts + 1;
+  answerStreak = 0;
   alert("das wrong broke boi")
 }
 
